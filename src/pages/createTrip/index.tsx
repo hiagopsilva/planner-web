56ab811dd7e550161558694fb01cc2d97a36bd70
Hiago Prates <hiagohps360@gmail.com>
2024-07-27 22:20:33 -0300

    feat: destination and date step

--- src/pages/createTrip/index.tsx
+++ src/pages/createTrip/index.tsx
@@ -1,14 +1,9 @@
-import {
-  ArrowRight,
-  Calendar,
-  MapPin,
-  Settings2,
-  UserRoundPlus,
-} from 'lucide-react'
+import { ArrowRight, UserRoundPlus } from 'lucide-react'
 import { FormEvent, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { InviteGuestsModal } from './invite-guests-modal'
 import { ConfirmTripModal } from './confirm-trip-modal'
+import { DestinationAndDateStep } from './steps/destination-and-date-step'
 
 export function CreateTrip() {
   const navigate = useNavigate()
@@ -79,47 +74,11 @@ export function CreateTrip() {
         </div>
 
         <div className="space-y-4">
-          <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
-            <div className="flex items-center gap-2 flex-1">
-              <MapPin className="size-5 text-zinc-400" />
-              <input
-                disabled={isGuestInputOpen}
-                type="text"
-                placeholder="Pra onde você vai?"
-                className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
-              />
-            </div>
-
-            <div className="flex items-center gap-2">
-              <Calendar className="size-5 text-zinc-400" />
-              <input
-                disabled={isGuestInputOpen}
-                type="text"
-                placeholder="Quando?"
-                className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none"
-              />
-            </div>
-
-            <div className="w-px h-6 bg-zinc-800"></div>
-
-            {isGuestInputOpen ? (
-              <button
-                onClick={closeGuestInput}
-                className="bg-zinc-800 text-zinc-200 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-zinc-700"
-              >
-                Alterar local/data
-                <Settings2 />
-              </button>
-            ) : (
-              <button
-                onClick={openGuestInput}
-                className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400"
-              >
-                Continuar
-                <ArrowRight className="size-5" />
-              </button>
-            )}
-          </div>
+          <DestinationAndDateStep
+            closeGuestInput={closeGuestInput}
+            isGuestInputOpen={isGuestInputOpen}
+            openGuestInput={openGuestInput}
+          />
 
           {isGuestInputOpen && (
             <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
