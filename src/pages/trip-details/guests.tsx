9bfad17a290019487664cf9f880198311cac7656
Hiago Prates <hiagohps360@gmail.com>
2024-07-29 15:16:19 -0300

    feat: get participants data api

--- src/pages/trip-details/guests.tsx
+++ src/pages/trip-details/guests.tsx
@@ -1,30 +1,57 @@
-import { CircleDashed, UserCog } from 'lucide-react'
+import { CheckCircle2, CircleDashed, UserCog } from 'lucide-react'
 import { Button } from '../../components/button'
+import { useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { api } from '../../lib/axios'
 
+interface Participant {
+  id: string
+  name: string
+  email: string
+  is_confirmed: boolean
+}
 export function Guests() {
+  const { tripId } = useParams()
+
+  const [participant, setParticipant] = useState<Participant[]>([])
+
+  async function getParticipantData() {
+    const response = await api.get(`/trips/${tripId}/participants`)
+
+    setParticipant(response.data.participants)
+  }
+
+  useEffect(() => {
+    getParticipantData()
+  }, [])
+
   return (
     <div className="space-y-6">
       <h2 className="font-semibold text-xl">Convidados</h2>
 
       <div className="space-y-5">
-        {[
-          { name: 'Hiago Prates', email: 'hiago@gmail.com' },
-          { name: 'Daniella Prates', email: 'daniella@gmail.com' },
-        ].map((item, index) => (
-          <div key={index} className="flex items-center justify-between gap-4">
+        {participant.map((participant, index) => (
+          <div
+            key={participant.id}
+            className="flex items-center justify-between gap-4"
+          >
             <div className="space-y-1.5">
               <span className="block font-medium text-zinc-100">
-                {item.name}
+                {participant.name ?? `Convidado ${index}`}
               </span>
 
               <a
                 href="#"
                 className="block text-xs text-zinc-400 truncate hover:text-zinc-200"
               >
-                {item.email}
+                {participant.email}
               </a>
             </div>
-            <CircleDashed className="size-5 text-zinc-400 shrink-0" />
+            {participant.is_confirmed ? (
+              <CheckCircle2 className="size-5 text-green-400 shrink-0" />
+            ) : (
+              <CircleDashed className="size-5 text-zinc-400 shrink-0" />
+            )}
           </div>
         ))}
       </div>
